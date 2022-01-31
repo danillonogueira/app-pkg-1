daed629e02366fc07b6de50258d6a304d1f90d48
Danillo Nogueira <dnll_nog@yahoo.com>
2022-01-31 18:25:33 -0300

    initial commit

---
+++ src/components/Table.vue
@@ -0,0 +1,34 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th colspan="2">The table header</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>The table body</td>
+        <td>with two columns</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'Table',
+}
+</script>
+
+<style scoped lang="scss">
+  table,
+  td {
+    border: 1px solid #333;
+  }
+
+  thead,
+  tfoot {
+    background-color: #333;
+    color: #fff;
+  }
+</style>
